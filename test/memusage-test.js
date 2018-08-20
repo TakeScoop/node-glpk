92fcf79ba9cfec0612730ec4a1517d8bd0a5e69d
Jamie Beverly <jbeverly@users.noreply.github.com>
2018-08-20 13:53:05 -0700

    update with temporary git tag for cpp-eventemitter (#9)
    * update git tag for cpp-eventemitter to fix leak there.
    * add tests confirming leak is fixed here.

--- test/memusage-test.js
+++ test/memusage-test.js
@@ -1,10 +1,13 @@
 'use strict;'
 
+const iterate = require('leakage').iterate
 const expect = require('code').expect
 const testRoot = require('path').resolve(__dirname, '..')
 const glp = require('bindings')({ module_root: testRoot, bindings: 'glpk' })
 const temp = require('temp').track()
 const fs = require('fs')
+const Promise = global.Promise
+const setupSimplexLP = require('./setup_simplex').setupSimplexLP
 
 glp.termOutput(false)
 
@@ -27,6 +30,33 @@ describe('Verify meminfo', function() {
             }
         })
         lp.intopt({ msgLev: glp.MSG_ALL, presolve: glp.ON }, function() { })
+    })
+})
+
+describe('Leakage check', function() {
+    it('Should not leak memory', function() {
+        this.timeout(15000)
+        return iterate.async(() => {
+            return new Promise((resolve, reject) => {
+                let lp = setupSimplexLP()
+                let idx = 0;
+                lp.on('log', function(msg) {
+                    let info = glp.glpMemInfo()
+                    expect(info).to.be.an.object()
+                    expect(info).to.include(['count','cpeak','total','tpeak'])
+                    expect(info.count).to.be.a.number()
+                    expect(info.cpeak).to.be.a.number()
+                    expect(info.total).to.be.a.number()
+                    expect(info.tpeak).to.be.a.number()
+                    expect(info.tpeak).to.be.at.least(info.total)
+                    expect(info.cpeak).to.be.at.least(info.count)
+                })
+
+                lp.intopt({ msgLev: glp.MSG_ALL, presolve: glp.ON }, function() {
+                    resolve()
+                })
+            })
+        })
     })
 })
 
